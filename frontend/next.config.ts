507dc32dea83dc3dca6c6d449bd9416426cd86a5
Muhammad Taimoor <111456485+TaimoorBasit@users.noreply.github.com>
2026-02-19 22:25:51 +0500

    chore: enable external image sources

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,9 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
       {
         protocol: 'http',
