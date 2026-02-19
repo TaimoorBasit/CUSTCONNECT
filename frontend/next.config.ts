e01c24d52a85ba562af772745bf55efef56e9ee2
Muhammad Taimoor <111456485+TaimoorBasit@users.noreply.github.com>
2026-02-19 23:03:38 +0500

    fix(frontend): remove eslint config from next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -23,11 +23,7 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: false,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: false,
-  },
+
 } satisfies NextConfig;
 
 export default nextConfig;
